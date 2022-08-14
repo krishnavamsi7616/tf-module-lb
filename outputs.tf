2bdf57f774f6be29192d5122e3002e290972cfe9
Krishna Vamsi <nvgkvamsi@gmail.com>
2022-08-13 20:30:16 -0700

    updating ./terraform/tf-module-mutable-lb

--- outputs.tf
+++ outputs.tf
@@ -10,10 +10,6 @@ output "PRIVATE_LB_DNS" {
   value = aws_lb.private.dns_name
 }
 
-output "PUBLIC_LB_DNS" {
-  value = aws_lb.public.dns_name
-}
-
 output "PRIVATE_LISTENER_ARN" {
   value = aws_lb_listener.backend.arn
 }
--- private-lb.tf
+++ private-lb.tf
@@ -1,6 +1,6 @@
 resource "aws_lb" "private" {
   name               = "${var.PROJECT}-${var.ENV}-alb-private"
-  internal           = false
+  internal           = true
   load_balancer_type = "application"
   security_groups    = [aws_security_group.private-sg.id]
   subnets            = var.PRIVATE_SUBNET_IDS
